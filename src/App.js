eafe384a37031aff453adbc5da56831dbd3b1dde
Angela <nabaweesi3579@gmail.com>
2023-04-08 15:08:41 +0300

    ecommerce initial push

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound'
+import { BrowserRouter }  from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path= "/cart" element={<Cart />} />
+          <Route path="/notfound" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
